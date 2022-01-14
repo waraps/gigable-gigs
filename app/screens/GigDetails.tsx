df5d6ba9266794f15db6ab4ca12a1568c7e26217
Manuel Meneses <manuelmenesesg@hotmail.com>
2022-01-14 13:24:25 -0400

    fix categories bugs + refactor

--- app/screens/GigDetails.tsx
+++ app/screens/GigDetails.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text, Image, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
 // API
@@ -29,7 +36,11 @@ const GigDetails = ({ route }: Props) => {
     try {
       setLoading(true);
       const response: IGig = await APIGigs.getOne(id);
-      setGig(response);
+      const formattedDates = {
+        startingDate: new Date(response.startDate.date),
+        endingDate: new Date(response.endDate.date),
+      };
+      setGig({ ...response, ...formattedDates });
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -39,123 +50,141 @@ const GigDetails = ({ route }: Props) => {
 
   if (loading)
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={styles.loadingContainer}>
         <ActivityIndicatorDetails />
-        <Text style={{ marginTop: 15, fontSize: 18 }}>Loading data ...</Text>
+        <Text style={styles.loadingContainerText}>Loading data ...</Text>
       </View>
     );
 
   return (
-    <View style={styles.container}>
-      <View style={styles.sectionOne}>
-        <View style={{ flexDirection: "row" }}>
-          <Image
-            style={styles.avatar}
-            source={{
-              uri: gig?.user.avatar,
-            }}
-          />
-          <View style={styles.infoContainer}>
-            <Text style={styles.name}>{gig?.user.displayname}</Text>
-            <Text style={styles.description}>{gig?.tags[0].name}</Text>
-          </View>
-        </View>
-        <View>
-          <Text
-            style={[
-              { backgroundColor: gig?.active ? "#00D1B2" : "#E64743" },
-              styles.status,
-            ]}
-          >
-            {gig?.status ? "AVAILABLE" : "UNAVAILABLE"}
-          </Text>
-          <Text>#{gig?.id}</Text>
-        </View>
-      </View>
-      {gig && gig.deliveryGig.categories.length > 0 && (
-        <View>
-          <Text>Delivery Driver Categories:</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.sectionOne}>
           <View style={{ flexDirection: "row" }}>
-            {gig?.deliveryGig.categories.map((category) => {
-              return (
-                <Text key={category.id} style={styles.pill}>
-                  {category.name}
+            <Image
+              style={styles.avatar}
+              source={{
+                uri: gig?.user.avatar,
+              }}
+            />
+            <View style={styles.infoContainer}>
+              <Text style={styles.name}>{gig?.user.displayname}</Text>
+              <Text style={styles.description}>{gig?.tags[0].name}</Text>
+            </View>
+          </View>
+          <View>
+            <Text
+              style={[
+                { backgroundColor: gig?.active ? "#00D1B2" : "#E64743" },
+                styles.status,
+              ]}
+            >
+              {gig?.status ? "AVAILABLE" : "UNAVAILABLE"}
+            </Text>
+            <Text style={{marginLeft: 4}}>#{gig?.id}</Text>
+          </View>
+        </View>
+        {gig?.deliveryGig?.categories &&
+          gig?.deliveryGig?.categories.length > 0 && (
+            <View>
+              <Text>Delivery Driver Categories:</Text>
+              {
+                <View style={{ flexDirection: "row" }}>
+                  {gig?.deliveryGig.categories.map((category) => {
+                    return (
+                      <Text key={category.id} style={styles.pill}>
+                        {category.name}
+                      </Text>
+                    );
+                  })}
+                </View>
+              }
+            </View>
+          )}
+        <View style={{ marginVertical: 10 }}>
+          <Text>{gig?.description}</Text>
+        </View>
+        <View style={styles.sectionTwo}>
+          <Text style={styles.subtitle}>Dates</Text>
+          <View style={styles.sectionTwoDates}>
+            <View>
+              <Text style={{ fontWeight: "bold" }}>From</Text>
+              <Text>{gig?.startingDate?.toDateString()}</Text>
+              <Text style={{ fontWeight: "bold" }}>To</Text>
+              <Text>{gig?.endingDate?.toDateString()}</Text>
+            </View>
+            <View>
+              <Text style={{ fontWeight: "bold" }}>Working hours</Text>
+              <Text>
+                {gig?.startDate.time} - {gig?.endDate.time}
+              </Text>
+            </View>
+          </View>
+          <Text style={styles.subtitle}>Rates</Text>
+          <View style={{ flexDirection: "row", marginBottom: 10 }}>
+            {gig && gig?.costHour > 0 && (
+              <View>
+                <Text style={{ fontWeight: "bold", marginRight: 30 }}>
+                  Per hour
                 </Text>
-              );
-            })}
+                <Text>
+                  {gig?.currency.symbol} {gig?.costHour}{" "}
+                </Text>
+              </View>
+            )}
+            {gig && gig?.costActivity > 0 && (
+              <View style={{ marginRight: 30 }}>
+                <Text style={{ fontWeight: "bold" }}>Per delivery</Text>
+                <Text>
+                  {gig?.currency.symbol} {gig?.costActivity}{" "}
+                </Text>
+              </View>
+            )}
+            {gig && gig?.costGig > 0 && (
+              <View>
+                <Text style={{ fontWeight: "bold" }}>Per km</Text>
+                <Text>
+                  {gig?.currency.symbol} {gig?.costGig}{" "}
+                </Text>
+              </View>
+            )}
           </View>
-        </View>
-      )}
-      <View>
-        <Text>{gig?.description}</Text>
-      </View>
-      <View style={styles.sectionTwo}>
-        <Text style={styles.subtitle}>Dates</Text>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          <View>
-            <Text style={{ fontWeight: "bold" }}>From</Text>
-            <Text>{gig?.startDate.timestamp.toString()}</Text>
-            <Text style={{ fontWeight: "bold" }}>To</Text>
-            <Text>{gig?.endDate.date}</Text>
-          </View>
-          <View>
-            <Text style={{ fontWeight: "bold" }}>Working hours</Text>
-            <Text>21:30 to 21:30</Text>
-          </View>
-        </View>
-        <Text style={styles.subtitle}>Rates</Text>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          {gig && gig?.costHour > 0 && (
-            <View>
-              <Text style={{ fontWeight: "bold" }}>Per hour</Text>
-              <Text>
-                {gig?.currency.symbol} {gig?.costHour}{" "}
-              </Text>
-            </View>
-          )}
-          {gig && gig?.costActivity > 0 && (
-            <View>
-              <Text style={{ fontWeight: "bold" }}>Per delivery</Text>
-              <Text>
-                {gig?.currency.symbol} {gig?.costActivity}{" "}
-              </Text>
-            </View>
-          )}
-          {gig && gig?.costGig > 0 && (
-            <View>
-              <Text style={{ fontWeight: "bold" }}>Per km</Text>
-              <Text>
-                {gig?.currency.symbol} {gig?.costGig}{" "}
-              </Text>
-            </View>
-          )}
-        </View>
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: gig ? gig.lat : 0,
-            longitude: gig ? gig.lng : 0,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-          <Marker
-            coordinate={{
+          <MapView
+            style={styles.map}
+            initialRegion={{
               latitude: gig ? gig.lat : 0,
               longitude: gig ? gig.lng : 0,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
             }}
-          />
-        </MapView>
-        <Text style={{ fontWeight: "bold" }}>Pick-up location</Text>
-        <Text>{gig?.address.formatted_address}</Text>
-        <Text style={{ fontWeight: "bold" }}>Drop-off location</Text>
-        <Text>{gig?.address.formatted_address}</Text>
+          >
+            <Marker
+              coordinate={{
+                latitude: gig ? gig.lat : 0,
+                longitude: gig ? gig.lng : 0,
+              }}
+            />
+          </MapView>
+          <Text style={{ fontWeight: "bold" }}>Pick-up location</Text>
+          <Text>{gig?.address.formatted_address}</Text>
+          <Text style={{ fontWeight: "bold" }}>Drop-off location</Text>
+          <Text>{gig?.address.formatted_address}</Text>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loadingContainerText: {
+    marginTop: 15,
+    fontSize: 18,
+  },
   container: {
     flex: 1,
     padding: 16,
@@ -196,6 +225,10 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   sectionTwo: {},
+  sectionTwoDates: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
   subtitle: {
     fontSize: 25,
   },
